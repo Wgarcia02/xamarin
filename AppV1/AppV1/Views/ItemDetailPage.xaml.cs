c87e80f80ca8a43f9b1e3cd2dcf0edb43e860b58
Juan Carlos Martinez Diaz <jcmartinezdi@cesde.edu.co>
2019-03-01 17:07:36 -0500

    HolaMundo

---
+++ AppV1/AppV1/Views/ItemDetailPage.xaml.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+using AppV1.Models;
+using AppV1.ViewModels;
+
+namespace AppV1.Views
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class ItemDetailPage : ContentPage
+    {
+        ItemDetailViewModel viewModel;
+
+        public ItemDetailPage(ItemDetailViewModel viewModel)
+        {
+            InitializeComponent();
+
+            BindingContext = this.viewModel = viewModel;
+        }
+
+        public ItemDetailPage()
+        {
+            InitializeComponent();
+
+            var item = new Item
+            {
+                Text = "Item 1",
+                Description = "This is an item description."
+            };
+
+            viewModel = new ItemDetailViewModel(item);
+            BindingContext = viewModel;
+        }
+    }
+}
---
+++ HolaMundo/MainActivity.cs
@@ -0,0 +1,28 @@
+﻿using Android.App;
+using Android.OS;
+using Android.Support.V7.App;
+using Android.Runtime;
+using Android.Widget;
+using Android.Content;
+
+namespace HolaMundo
+{
+    [Activity(Label = "HolaMundoApp", MainLauncher = true)]
+    public class MainActivity : AppCompatActivity
+    {
+        protected override void OnCreate(Bundle savedInstanceState)
+        {
+            base.OnCreate(savedInstanceState);
+            // Set our view from the "main" layout resource
+            SetContentView(Resource.Layout.activity_main);
+            HacerTexto(this, "Bienvenido a mi primera aplicación");
+
+
+        }
+        public void HacerTexto(Context ctx, string str)
+        {
+            Toast.MakeText(ctx, str, ToastLength.Long).Show();
+        }
+
+    }
+}
